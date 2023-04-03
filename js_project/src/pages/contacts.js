e285eb6325f517327437cebaad7e8325ebb09830
MInesBarbosaa <93823221+MInesBarbosaa@users.noreply.github.com>
2023-04-03 18:00:27 +0100

    Contacts page

--- js_project/src/pages/contacts.js
+++ js_project/src/pages/contacts.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import contactfig from './dog_letter.webp';
   
 const Contact = () => {
+
+  const [userText, setUserText] = useState('');
+
+  const textChange = (e) => {
+    setUserText(e.target.value);
+  }
+
+  const textSubmit = () => {
+    let userTextSave = userText;
+    console.log(userText);
+    setUserText("");
+    alert('Your message was submitted. Thank you for your contact!') 
+  }
+
+
   return (
     <div>
       <div className='contacts'>
-        <h1>We are glad to see you on our page! And we will be happy to advise you on feeding and caring for your pets! 
-            Feel free to contact us on the following email: 
-          <br></br>
-          <br></br>
-          DogLoversClub@gmail.com
-        </h1>
+        <h1>We are glad to see you on our page! We will be happy to advise you on feeding and caring for your pets!</h1> 
       </div>
 
-      <div className='dogMail'>
-            <img src={contactfig} alt="MBI Calculator Logo"></img>
+      <div className='contactUser'>
+        <div className='dogMail'>
+          <img src={contactfig} alt="Dog with a Letter"></img>
+        </div>
+        
+        <div className="userText">
+          <h1 id="userHeader">Feel free to contact us using the box below:</h1>
+          <textarea id="userBox" onChange={textChange} value={userText} placeholder="Write something to us..."></textarea>
+          <button id="userButton" onClick={textSubmit}>Submit</button>
+        </div>
       </div>
-      
+
       <footer>
         	<p>© 2023 Dog Lovers Inc. All Rights Reserved.</p>
       </footer>
